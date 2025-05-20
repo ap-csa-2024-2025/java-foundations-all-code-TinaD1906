cf42c8a0552412bd11bf6c2e83cf702ebe6c397f
TinaD1906 <dotina87@gmail.com>
2025-05-20 17:17:32 +0000

    yayaya

--- src/jfo-section-1/HelloWorld.java
+++ src/jfo-section-1/HelloWorld.java
@@ -2,5 +2,38 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int a = 10000;
+        byte y = 2;
+        short z = 1000;
+        long b = 1000000000000L;
+
+
+        int i = 0;
+        System.out.println("i loop");
+        while (i<10)
+        {
+            System.out.println(i++);
+        }
+
+        int j = 0;
+        System.out.println("j loop");
+        while (j<10)
+        {
+            System.out.println(++j);
+        }
+
+
+
+
+
+
+
+        int eggsPerChicken = 5;
+        int chickenCount = 3;
+
+
+        //monday 
+        
+
     }
 }
--- src/jfo-section-2/Text01.java
+++ src/jfo-section-2/Text01.java
@@ -1,5 +1,10 @@
 public class Text01 {
     public static void main(String[] args) {
+        double x = 0.1;
+        double y = 0.2;
+
+        double z = x+y;
+        System.out.println("Are the numbers equal"+(z==0.3));
         System.out.println("   /\\         /\\   ");
         System.out.println("  /  \\_______/  \\  ");
         System.out.println(" /               \\ ");
